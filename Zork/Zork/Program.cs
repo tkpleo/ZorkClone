875503dbeecbbab64487d6d7b849301cecd87b7c
tkpleo <tkristofferpurdy@gmail.com>
2025-10-06 17:59:13 -0400

    fixed a word

--- Zork/Zork/Program.cs
+++ Zork/Zork/Program.cs
@@ -68,7 +68,7 @@ namespace Zork
             roomMap["South of House"].Description = "You are facing the sout hside of a white house. There is no door here, and all the windows are barred.";
             roomMap["Canyon View"].Description = "You are at the top of the Great Canyon on its sout wall";
             roomMap["Forest"].Description = "This is a forest, with trees in all directions around you.";
-            roomMap["West of House"].Description = "This is an open field west of a white house, wit ha boarded front door.";
+            roomMap["West of House"].Description = "This is an open field west of a white house, with a boarded front door.";
             roomMap["Behind House"].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar.";
             roomMap["Dense Woods"].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";
             roomMap["North of House"].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";
