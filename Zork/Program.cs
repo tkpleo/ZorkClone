debf274233be6d204b5da9d4b26be3140f7ad9d3
tkpleo <tkristofferpurdy@gmail.com>
2025-10-06 00:06:20 -0400

    Initial upload

---
+++ Zork/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Data.Common;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Zork
+{
+    public static class Assert
+    {
+        [Conditional("DEBUG")]
+        public static void IsTrue(bool expression, string message = null)
+        {
+            if (expression == false)
+            {
+                throw new Exception(message);
+            }
+        }
+    }
+
+    public class Room
+    {
+        private static void InitializeRoomDescriptions()
+        {
+            var roomMap = new Dictionary<string, Room>();
+            foreach (Room room in Rooms)
+            {
+                roomMap[room.Name] = room;
+            }
+
+            roomMap["Rocky Trail"].Description = "You are on a rock-strewn trail.";
+            roomMap["South of House"].Description = "You are facing the sout hside of a white house. There is no door here, and all the windows are barred.";
+            roomMap["Canyon View"].Description = "You are at the top of the Great Canyon on its sout wall";
+            roomMap["Forest "]
+        }
+        public string Name { get; }
+        public override string ToString() => Name;
+
+        public string Description { get; set; }
+
+        public Room(string name, string description = "")
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+    enum Commands
+    {
+        QUIT,
+        LOOK,
+        NORTH,
+        SOUTH,
+        EAST,
+        WEST,
+        UNKNOWN
+    }
+    internal class Program
+    {
+        private static Room CurrentRoom
+        {
+            get
+            {
+                return Rooms[Location.Row, Location.Column];
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wlcome to Zork!");
+            InitializeRoomDescriptions();
+
+            Room previousRoom = null;
+            Commands command = Commands.UNKNOWN;
+            while (command != Commands.QUIT)
+            {
+                Console.WriteLine(CurrentRoom);
+                if (previousRoom != CurrentRoom)
+                {
+                    Console.WriteLine(CurrentRoom.Description);
+                    previousRoom = CurrentRoom;
+                }
+                Console.Write("> ");
+                command = ToCommand(Console.ReadLine().Trim());
+
+                switch (command)
+                {
+                    case Commands.QUIT:
+                        Console.WriteLine("Thank you for playing!");
+                        break;
+                    case Commands.LOOK:
+                        Console.WriteLine("A rubber mat saying 'Welcome to Zork!' lies by the door.");
+                        break;
+                    case Commands.NORTH:
+                    case Commands.SOUTH:
+                    case Commands.EAST:
+                    case Commands.WEST:
+                        if (Move(command) == false)
+                        {
+                            Console.WriteLine("The way is shut!");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Unknown command.");
+                        break;
+                }
+            }
+        }
+
+        private static bool Move(Commands command)
+        {
+            Assert.IsTrue(IsDirection(command), "Invalid direction.");
+
+            bool isValidMove = true;
+            switch (command)
+            {
+                case Commands.NORTH when Location.Row < Rooms.GetLength(0) - 1:
+                    Location.Row++;
+                    break;
+
+                case Commands.SOUTH when Location.Row > 0:
+                    Location.Row--;
+                    break;
+
+                case Commands.EAST when Location.Column < Rooms.GetLength(1) - 1:
+                    Location.Column++;
+                    break;
+
+                case Commands.WEST when Location.Column > 0:
+                    Location.Column--;
+                    break;
+
+                default:
+                    isValidMove = false;
+                    break;
+            }
+
+            return isValidMove;
+        }
+
+        private static Commands ToCommand(String commandString) =>
+            Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
+        private static bool IsDirection(Commands command) => Directions.Contains(command);
+
+        private static readonly Room[,] Rooms =
+        {
+            {new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") },
+            {new Room("Forest"), new Room("West of House"), new Room("Behind House") },
+            {new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
+        };
+
+        private static readonly List<Commands> Directions = new List<Commands>
+        {
+            Commands.NORTH,
+            Commands.SOUTH,
+            Commands.EAST,
+            Commands.WEST,
+        };
+
+        private static (int Row, int Column) Location = (1, 1);
+    }
+
+}
